133d4d1b0783388af939d6526a188d837dcc73da
JanakiramanB31 <jrbofficial31@gmail.com>
2024-10-07 23:21:18 +0530

    Update main.tf
    Added the vpc subnet etc.

--- main.tf
+++ main.tf
@@ -1,11 +1,105 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "server" {
-  ami           = "ami-0fff1b9a61dec8a5f" # US-EAST-AMI-ID
-  instance_type = "t2.micro"
-  vpc_security_group_ids= ["sg-77ed1554"]
-  tags = { 
-    Name = "jrbHcpinstance"
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "main-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "main-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_instance" "server" {
+  count         = 2
+  ami           = "ami-0ebfd941bbafe70c6" # Example AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = element([aws_subnet.subnet1.id, aws_subnet.subnet2.id], count.index % 2)
+
+  tags = {
+    Name = "Server-${count.index + 1}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = aws_instance.server[count.index].id
+  port             = 80
+}
+
+output "load_balancer_dns" {
+  value = aws_lb.main.dns_name
 }
