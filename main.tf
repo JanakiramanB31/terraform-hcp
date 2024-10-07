afca86bc6b303a400835ec4bc814de7bef14e0a5
JanakiramanB31 <jrbofficial31@gmail.com>
2024-10-07 07:19:17 +0530

    Update main.tf
    Updated ami id

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "server" {
-  ami           = "ami-0ebfd941bbafe70c6" # Example AMI ID
+  ami           = "ami-0fff1b9a61dec8a5f" # US-EAST-AMI-ID
   instance_type = "t2.micro"
 }
