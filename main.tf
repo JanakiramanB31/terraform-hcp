3a3ff62894e766339022d4dbc47c36c47aba1644
JanakiramanB31 <jrbofficial31@gmail.com>
2024-10-07 14:45:26 +0530

    Update main.tf
    Updated the security group

--- main.tf
+++ main.tf
@@ -4,4 +4,8 @@ provider "aws" {
 resource "aws_instance" "server" {
   ami           = "ami-0fff1b9a61dec8a5f" # US-EAST-AMI-ID
   instance_type = "t2.micro"
+  vpc_security_group_ids= ["sg-77ed1554"]
+  tags = { 
+    Name = "jrbHcpinstance"
+  }
 }
